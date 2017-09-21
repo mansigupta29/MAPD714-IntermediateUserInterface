b533787ceb86e45493fbb657cfa54befeb6dcbc8
Student <student@SETASs-Mac-mini.local>
2017-09-20 22:29:07 -0400

    partial functionality

--- MAPD714IntermediateUserInteractions/ViewController.swift
+++ MAPD714IntermediateUserInteractions/ViewController.swift
@@ -16,6 +16,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var numberTextField: UITextField!
     @IBOutlet weak var sliderLabel: UILabel!
+    @IBOutlet weak var leftSwitch: UISwitch!
+    @IBOutlet weak var rightSwitch: UISwitch!
+    @IBOutlet weak var doSomethingButton: UIButton!
     
     //INHERITED METHODS
     override func viewDidLoad() {
@@ -26,16 +29,45 @@ class ViewController: UIViewController {
    //ACTION METHODS
 
     @IBAction func onSliderChanged(_ sender: UISlider) {
+        sliderLabel.text = String(lroundf(sender.value))
     }
 
-    @IBAction func onSegmentedControlChanged(_ sender: Any) {
+    @IBAction func OnSegmentedControlChanged(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            leftSwitch.isHidden = false
+            rightSwitch.isHidden = false
+            doSomethingButton.isHidden = true
+            
+        }
+        else{
+            leftSwitch.isHidden = true
+            rightSwitch.isHidden = true
+            doSomethingButton.isHidden = false
+        }
     }
     
     @IBAction func onDoSomethingPressed(_ sender: UIButton) {
     }
     
     @IBAction func onSwitchChanged(_ sender: UISwitch) {
+        let setting = sender.isOn
+        leftSwitch.setOn(setting, animated: true)
+        rightSwitch.setOn(setting, animated: true)
+        
     }
+    
+    @IBAction func textFieldDoneEditing(_ sender: UITextField) {
+        sender.resignFirstResponder()
+    }
+    
+    @IBAction func onTapGestureRecognizer(_ sender: UITapGestureRecognizer) {
+        numberTextField.resignFirstResponder()
+        nameTextField.resignFirstResponder()
+    }
+    
+    @IBAction func doSOmethingPressed(_ sender: UIButton) {
+    }
+   
     
 }
 
